3aaad9349555053e211c012ccbe61cc191891048
Miji <mjkim2304@gmail.com>
2022-10-19 13:53:45 +0900

    Spring 적용

--- src/main/java/connectToDB/dao/UserDaoFactory.java
+++ src/main/java/connectToDB/dao/UserDaoFactory.java
@@ -1,8 +1,13 @@
-package connectToDB.toClass;
+package connectToDB.dao;
 
 import connectToDB.UserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class UserDaoFactory {
+
+    @Bean
     public UserDao awsUserDao(){
         UserDao userDao=new UserDao(new AwsConnectionMaker());
         return userDao;
--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -1,29 +1,32 @@
 import connectToDB.UserDao;
-import connectToDB.abstrat.AWSUserDao;
-import connectToDB.User;
-import connectToDB.abstrat.UserDaoAbstract;
-import connectToDB.toClass.AwsConnectionMaker;
-import connectToDB.toClass.UserDaoFactory;
+import domain.User;
+import connectToDB.dao.UserDaoFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes=UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
+
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-        //connectToDB.UserDao userDao = new connectToDB.UserDao();
+        //String id = "12"; // to prevent duplicate PK errors
 
-        UserDao userDao = new UserDaoFactory().awsUserDao();
+        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
+        userDao.add(new User("14", "mimi", "cat"));
 
-        String id = "11"; // to prevent duplicate PK errors
-        String name = "Gigi";
-
-        User user = new User(id,name, "cat");
-        userDao.add(user);
-
-        User selectedUser = userDao.findById(id);
-        Assertions.assertEquals(name, selectedUser.getName());
+        User selectedUser = userDao.findById("14");
+        Assertions.assertEquals("mimi", selectedUser.getName());
         Assertions.assertEquals("cat", selectedUser.getPassword());
     }
 }
